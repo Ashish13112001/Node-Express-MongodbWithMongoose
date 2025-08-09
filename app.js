5a10c9ab9124908959bbced15d0e98670923b635
Ashish13112001 <barthwal13nov@gmail.com>
2025-08-10 01:18:42 +0530

    express-2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fs = require('node:fs');
 const app = express();
 
 
+app.use(express.json()); // frontend s data json(string) formate m aata h to ye middleware usko jsObject m convert kar deta h. Eski vajah s ham req.body m data access kar sakte h
 // app.get('/', (req, res) => {
 //     res.status(200).json({
 //         message : 'Hello from server side'
@@ -23,6 +24,72 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours: tours
         }
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });    
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    const id = req.params.id * 1;
+
+    const tour = tours.find(el => el.id === id);
+    if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid Id'
+        });
+    }
+
+    res.status(201).json({
+        status: 'success',
+        data : {
+            tour: tour
+        }
+    });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) =>{
+
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid Id'
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            message: 'Data updated'
+        }
+    });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) =>{
+
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid Id'
+        });
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
     });
 });
 
