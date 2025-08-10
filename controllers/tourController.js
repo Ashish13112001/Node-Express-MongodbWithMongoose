2d2d2459acb380cbeddea0e2f135fa472e61b01d
Ashish13112001 <barthwal13nov@gmail.com>
2025-08-10 16:18:16 +0530

    express-6

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+// Param middleware example 
+exports.checkId = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  next(); // vese to jarurat nahi h yaha 
+}
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -37,12 +48,6 @@ exports.getTour = (req, res) => {
   const id = req.params.id * 1;
 
   const tour = tours.find((el) => el.id === id);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
 
   res.status(201).json({
     status: 'success',
@@ -53,12 +58,7 @@ exports.getTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
+  
   res.status(200).json({
     status: 'success',
     data: {
@@ -68,12 +68,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
 
   res.status(204).json({
     status: 'success',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,8 +3,19 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
+/*
+ -- Params Middleware -- 
+    In Node/Express, params middleware (app.param()) is a special middleware that runs only when a specific route parameter (like :id) is present in the URL, and it’s used to validate or preprocess that parameter before the main route handler runs.
+*/
+/*
+ -- simple example --
+router.param('id', (req, res, next, val) => {
+  console.log(`Tour id is : ${val}`);
+  next();
+});
+*/
 
-
+router.param('id',tourController.checkId);
 
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router
