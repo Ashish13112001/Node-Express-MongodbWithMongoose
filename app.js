c8f4cc05824295f202be9703d8c5c39755d19658
Ashish13112001 <barthwal13nov@gmail.com>
2025-08-10 14:45:36 +0530

    express-5

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const fs = require('node:fs');
 const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json()); // frontend s data json(string) formate m aata h to ye middleware usko jsObject m convert kar deta h. Eski vajah s ham req.body m data access kar sakte h
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+
 
 app.use(morgan('dev'));
 
@@ -17,138 +16,8 @@ app.use((req, res, next) => {
     next();
 });
 
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find((el) => el.id === id);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: tour,
-    },
-  });
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      message: 'Data updated',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-
-
-const getAllUsers = (req, res) => {
-    res.status(501).json({
-        message: 'This route is not define yet'
-    });
-}
-const createUsers = (req, res) => {
-    res.status(501).json({
-        message: 'This route is not define yet'
-    });
-}
-const getUsers = (req, res) => {
-    res.status(501).json({
-        message: 'This route is not define yet'
-    });
-}
-const updateUsers = (req, res) => {
-    res.status(501).json({
-        message: 'This route is not define yet'
-    });
-}
-const deleteUsers = (req, res) => {
-    res.status(501).json({
-        message: 'This route is not define yet'
-    });
-}
-
-
-
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter
-  .route('/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-
-userRouter.route('/').get(getAllUsers).post(createUsers);
-userRouter
-.route('/:id')
-.get(getUsers)
-.patch(updateUsers)
-.delete(deleteUsers);
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 
-
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+
+
+exports.getAllUsers = (req, res) => {
+    res.status(501).json({
+        message: 'This route is not define yet'
+    });
+}
+exports.createUsers = (req, res) => {
+    res.status(501).json({
+        message: 'This route is not define yet'
+    });
+}
+exports.getUsers = (req, res) => {
+    res.status(501).json({
+        message: 'This route is not define yet'
+    });
+}
+exports.updateUsers = (req, res) => {
+    res.status(501).json({
+        message: 'This route is not define yet'
+    });
+}
+exports.deleteUsers = (req, res) => {
+    res.status(501).json({
+        message: 'This route is not define yet'
+    });
+}
